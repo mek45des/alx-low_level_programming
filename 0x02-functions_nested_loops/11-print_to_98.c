fd64d3a6569bc4088de51ee0b4a3073fc29501c2
mek45des <mekdishaadamu@gmail.com>
2023-06-21 05:02:40 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,16 +13,14 @@ void print_to_98(int n)
 	{
 		for (; n > 98; n--)
 		{
-			_putchar(n);
-			_putchar(',');
+			_putchar((n / 10) + '0');
 		}
 	}
 	else if (n < 98)
 	{
 		for (; n < 98; n++)
 		{
-			_putchar(n);
-			_putchar(',');
+			_putchar((n % 10) + '0');
 		}
 
 	}
