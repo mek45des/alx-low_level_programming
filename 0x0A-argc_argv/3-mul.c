91134894a77778513fa6559a24d120028b22aa69
mek45des <mekdishaadamu@gmail.com>
2023-07-07 15:37:39 -0700

    0x0a-argc_argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers.
+ * @argc: number of command line argument
+ * @argv: array that contains the program comand line
+ * Return: 0 -success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]), * atoi(argv[2]));
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,5 @@
+#ifndef main_h
+#define main_h
+int _putchar(char);
+int _atoi(char *s);
+#endif
