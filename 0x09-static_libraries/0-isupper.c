20b1897f134efc127f8d4f9d0ea29a89d772f372
mek45des <mekdishaadamu@gmail.com>
2023-07-07 06:04:46 -0700

    libmy.a, main.h

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - cheks if the parammetr is upercase character
+ * @c: input character
+ * Return: 1 if it's uppercase character, 0 in another
+ */
+int _isupper(int c)
+{
+        if (c >= 65 && c <= 90)
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
+}
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - shows 1 if the input is a
+ * letter another case,shows 0
+ *
+ * @c: the character in ASCII code
+ *
+ * Return: 1 for letters, 0 for the rest
+ */
+int _isalpha(int c)
+{
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
+}
