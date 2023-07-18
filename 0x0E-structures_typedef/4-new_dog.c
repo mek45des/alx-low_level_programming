1ca85cc782c6e41cb557716eb8ffdf276982b302
mek45des <mekdishaadamu@gmail.com>
2023-07-17 22:46:39 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -37,7 +37,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (i = 0; i < lname; i++)
 		p_dog->name[i] = name[i];
-	p_dog->name = '\0';
+	p_dog->name[i] = '\0';
 
 	p_dog->age = age;
 
